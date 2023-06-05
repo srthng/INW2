eca1e5f719ec3f94dd0bdac01b1d345799cc8988
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 12:23:29 -0300

    temperatura

---
+++ exemplosforms/oi.js
@@ -0,0 +1,21 @@
+//console.log("Nome\tNota\tTurma")
+//console.log("Pedro\t7.5\tA")
+//console.log("Maria\t8\tC")
+//console.log("José\t9\tB")
+
+//let nome = "Pedro"
+//let valor1 = 10
+//let valor2 = 20
+//let valor3 = valor1 + valor2
+//console.log("Oi meu amigo " + nome)
+//console.log("Total do valor " +valor3)
+
+const leia = require("prompt-sync")()
+
+
+
+temperatura = parseInt(leia("Digite a temperatura desejada : "))
+
+Temperatura2 = temperatura * temperatura
+
+console.log("Temperatura ao quadrado total : " + Temperatura2)
