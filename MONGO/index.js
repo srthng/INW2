7eae34b87c79caa6188d86ed27efe26fcc3cabf2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-06 08:39:45 -0300

    Mongoose

--- MONGO/index.js
+++ MONGO/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017', 
-{userNewUrlParser : true, useUnifiedTopology: true, serverSelectionTimeoutMS : 10000}
+{
+    userNewUrlParser : true, 
+    useUnifiedTopology: true, 
+    serverSelectionTimeoutMS : 10000}
 );
 
 const db=mongoose.connection;
@@ -14,3 +17,25 @@ db.once('open', function()
 }
 );
 
+const pessoaSchema = new mongoose.Schema( {
+        nome: String,
+        idade: Number,
+        profissao: String
+    }
+);
+
+const Pessoa = mongoose.model("Pessoa", pessoaSchema);
+
+const marcos = new Pessoa({
+     
+    Nome: 'Marcos',
+    Idade: 25,
+    profissao: 'Programador'
+    }
+);
+
+
+console.log(marcos.Nome);
+console.log(marcos.idade);
+console.log(marcos.profissao);
+marcos.save();
